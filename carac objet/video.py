7a0b6c4f943a568fb8c39ab6eefb674b63990725
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-04 19:50:39 +0100

    Add files via upload

---
+++ carac objet/video.py
@@ -0,0 +1,8 @@
+
+class video:
+    
+    def ouvrir_video(self):
+
+        self.lien = "https://www.google.fr/search?source=hp&ei=5OIPXMv1IY-GwPAPzauD6A0&q=une+voiture+video&btnK=Recherche+Google&oq={}+video&gs_l=psy-ab.3..35i39j0i22i30l3.683.2129..2211...0.0..0.211.1871.4j12j1......0....1..gws-wiz.....0..0j0i67j0i131j0i20i263j0i22i10i30.PpM4Ky2FBoo"
+    
+    
